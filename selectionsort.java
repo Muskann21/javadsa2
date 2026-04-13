dff88535ded76dc082489f8754505e8c769c893b
Muskann21 <muskansingal21@gmail.com>
2026-04-13 21:34:32 +0530

    Create selectionsort.java

---
+++ selectionsort.java
@@ -0,0 +1,11 @@
+public class selectionsort {
+    public static void selection(){
+        int[] arr={7,3,8,2,5,4};
+        int first=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if (arr[i]<first){
+                
+            }
+        }
+    }
+}
