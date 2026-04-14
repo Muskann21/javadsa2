d898bd3d75eeb56e2ab2a9f3c328448923c1e49c
Muskann21 <muskansingal21@gmail.com>
2026-04-14 18:05:17 +0530

    Update selectionsort.java

--- selectionsort.java
+++ selectionsort.java
@@ -1,11 +1,23 @@
 public class selectionsort {
     public static void selection(){
         int[] arr={7,3,8,2,5,4};
-        int first=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if (arr[i]<first){
-                first=arr[i];
+        for(int i=0;i<arr.length;i++){
+            int first=i;
+          for(int j=i+1;j<arr.length;j++){
+            if (arr[j]<arr[first]){
+                first=j;
             }
         }
+        int temp=arr[first];
+        arr[first]=arr[i];
+        arr[i]=temp;
     }
+
+    for(int k=0;k<arr.length;k++){
+            System.out.println(arr[k]+" ");
+        }
+}
+public static void main(String[] args) {
+    selection();
+}
 }
