dd450b492d16e7ac0b2d71b2c060fda9c034130d
Muskann21 <muskansingal21@gmail.com>
2026-04-20 21:40:01 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+public class stack {
+    public static void main(String[] args) {
+        Stack<String> sc=new Stack<>();
+        sc.push("M");
+        sc.push("u");
+        sc.push("s");
+        sc.push("k");
+        sc.push("a");
+        sc.push("n");
+        System.out.print(sc);
+    }
+}
