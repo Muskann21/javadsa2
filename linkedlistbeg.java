6986dbccf544961839d9f1ba81825ced2e32f0af
Muskann21 <muskansingal21@gmail.com>
2026-04-30 17:58:40 +0530

    Update linkedlistbeg.java

--- linkedlistbeg.java
+++ linkedlistbeg.java
@@ -10,13 +10,45 @@ class Link{
     Node head;
     void insert(int data){
         Node newNode=new Node(data);
-    if(head==null){
+        newNode.next=head;
         head=newNode;
-        return;
     }
-    Node temp=head;
-    head=newNode;
-    newNode=temp.next;
+    void insertmid(int data){
+        Node newNode=new Node(data);
+        Node temp=head;
+        int x=0;
+        while(temp!=null){
+            x+=1;
+            temp=temp.next;
+        }
+        Node temp2=head;
+        int y=0;
+        while(y!=x/2){
+            temp2=temp2.next;
+            y+=1;
+        }
+        newNode.next=temp2.next;
+    }
+    void deleteb(){
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        Node temp=head;
+        head=temp.next;
+        temp=null;
+
+    }
+    void deletel(){
+        if(head.next==null){
+            head=null;
+            return;
+        }
+       Node temp=head;
+       while(temp.next.next!=null){
+        temp=temp.next;
+       }
+       temp.next=null;
     }
     void display(){
         Node temp=head;
@@ -33,6 +65,9 @@ public class linkedlistbeg {
         list.insert(23);
         list.insert(73);
         list.insert(46);
+        list.insert(56);
+        list.insert(321);
+        list.deleteb();
         list.display();
     }
 }
